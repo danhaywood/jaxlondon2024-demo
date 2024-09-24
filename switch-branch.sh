82aef9dffedb8f57586e3434d24b8def80cad36e
Dan Haywood <dan@haywood-associates.co.uk>
2024-09-24 22:33:44 +0100

    makes switch-branch one-way

--- switch-branch.sh
+++ switch-branch.sh
@@ -1,86 +1,19 @@
 #!/usr/bin/env bash
 
-# Function to display help message
-show_help() {
-    echo "Usage: $0 [-3 | -2]"
-    echo ""
-    echo "Options:"
-    echo "  -3  Switch to v3 (v3.marker files etc)"
-    echo "  -2  Switch back to v2"
-    echo "  -h  Show this help message"
-}
-
-# Default action
-ACTION=""
-
-# Process the command-line options
-while getopts ":23h" opt; do
-  case $opt in
-    2)
-      ACTION="v2"
-      ;;
-    3)
-      ACTION="v3"
-      ;;
-    h)
-      show_help
-      exit 0
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      show_help
-      exit 1
-      ;;
-  esac
-done
-
-# Check if an action has been selected
-if [ -z "$ACTION" ]; then
-    echo "Error: You must specify either -2 or -3 to indicate the branch"
-    show_help
-    exit 1
-fi
-
 # Function to create marker file
 create_marker() {
     pomFile=$1
     pomDir=$(dirname "$pomFile")
     markerFile="$pomDir/v3.marker"
 
-    # Skip directories with pipeline-resources in their path
-    echo "$pomDir" | grep -v pipeline-resources >/dev/null
-    if [ $? -eq 0 ]; then
-        echo "adding marker file ${markerFile}"
-        echo "marker file to activate v3 Maven profile" > "$markerFile"
-    fi
+    echo "adding marker file ${markerFile}"
+    echo "marker file to activate v3 Maven profile" > "$markerFile"
 }
 
-# Function to remove marker file
-remove_marker() {
-    pomFile=$1
-    pomDir=$(dirname "$pomFile")
-    markerFile="$pomDir/v3.marker"
+for pomFile in $(find . -name pom.xml -print); do
+  create_marker "$pomFile"
+done
 
-    # Skip directories with pipeline-resources in their path
-    echo "$pomDir" | grep -v pipeline-resources >/dev/null
-    if [ $? -eq 0 ] && [ -f "$markerFile" ]; then
-        echo "removing marker file ${markerFile}"
-        rm "$markerFile"
-    fi
-}
-
-# Main logic for either creating or removing marker files
-if [ "$ACTION" == "v3" ]; then
-    for pomFile in $(find . -name pom.xml -print); do
-      create_marker "$pomFile"
-    done
-    mvn versions:update-parent -DparentVersion=3.1.0 -DgenerateBackupPoms=false
-    mvn versions:set -DnewVersion=3.1.0-SNAPSHOT -DgenerateBackupPoms=false
-elif [ "$ACTION" == "v2" ]; then
-    for pomFile in $(find . -name pom.xml -print); do
-      remove_marker "$pomFile"
-    done
-    mvn versions:update-parent -DparentVersion=2.1.0 -DgenerateBackupPoms=false
-    mvn versions:set -DnewVersion=2.1.0-SNAPSHOT -DgenerateBackupPoms=false
-fi
-
+mvn versions:update-parent -DparentVersion=3.1.0 -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=3.1.0-SNAPSHOT -DgenerateBackupPoms=false
+mvn rewrite:run

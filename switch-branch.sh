7f14fdbb2fa7af6a5174e89db6d4783aa41c5a06
Dan Haywood <dan@haywood-associates.co.uk>
2024-09-29 13:32:13 +0100

    makes switch-branch.sh more succinct

--- switch-branch.sh
+++ switch-branch.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
-# Function to create marker file
 create_marker() {
-    pomFile=$1
-    pomDir=$(dirname "$pomFile")
-    markerFile="$pomDir/v3.marker"
-
+    markerFile="$(dirname "$1")/v3.marker"
     echo "adding marker file ${markerFile}"
     echo "marker file to activate v3 Maven profile" > "$markerFile"
 }
 
-
-mvn rewrite:run
-
-mvn versions:update-parent -DparentVersion=3.1.0 -DgenerateBackupPoms=false
+mvn rewrite:run && \
+mvn versions:update-parent -DparentVersion=3.1.0 -DgenerateBackupPoms=false && \
 mvn versions:set -DnewVersion=3.1.0-SNAPSHOT -DgenerateBackupPoms=false
 
-for pomFile in $(find . -name pom.xml -print); do
-  create_marker "$pomFile"
-done
+find . -name pom.xml -exec bash -c 'create_marker "$0"' {} \;
 
 mvnd install -DskipTests
+
